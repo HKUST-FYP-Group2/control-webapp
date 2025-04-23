9b2abf55cdcf17f7b5592ab86fe6e9fb68d46d06
succsuccsucc <109941602+succsuccsucc@users.noreply.github.com>
2025-04-24 01:00:54 +0800

    add cypress tests for all pages

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -19,6 +19,70 @@ describe('Login', () => {
       cy.get('h1').contains('Projectors');
     });
 
+    it('should register an account', () => {
+      // Start from the index page
+      cy.visit("https://localhost:3000");
+
+      // Find the button that goes to the register page and click it
+      cy.get('a[href*="/register"]').click();
+
+      // Find the input with id "email" and type in the new username
+      const randomUsername = Math.random().toString(36).substring(2,7);
+      cy.get("#email").type(randomUsername);
+
+      // Find the input with id "streamKey" and type in the new stream key
+      cy.get("#streamKey").type(`${randomUsername}_key`);
+
+      // Find the input with id "password" and type in the new password
+      cy.get("#password").type('password');
+
+      // Find the input with id "confirmPassword" and type in the new password again
+      cy.get("#confirmPassword").type('password');
+
+      // Find the button that submits the form and click it
+      cy.get('button[type*="submit"]').click();
+    });
+
+    it('should open the pair projector page', () => {
+      // Start from the index page
+      cy.visit('https://localhost:3000/');
+   
+      // Find the input with id "username" and type in the username
+      cy.get('#username').type('desmond');
+
+      // Find the input with id "password" and type in the password
+      cy.get('#password').type('password');
+
+      // Find the button that submits the form and click it
+      cy.get('button[type*="submit"]').click();
+   
+      // The new url should include "/dashboard"
+      cy.url().should('include', '/dashboard');
+
+      // Find the button that goes to the dashboard page and click it
+      cy.get('a[href*="/dashboard/pair"]').click();
+    });
+
+    it('should log in and log out', () => {
+      // Start from the index page
+      cy.visit('https://localhost:3000/');
+   
+      // Find the input with id "username" and type in the username
+      cy.get('#username').type('desmond');
+
+      // Find the input with id "password" and type in the password
+      cy.get('#password').type('password');
+
+      // Find the button that submits the form and click it
+      cy.get('button[type*="submit"]').click();
+   
+      // The new url should include "/dashboard"
+      cy.url().should('include', '/dashboard');
+
+      // Find the button that goes to the dashboard page and click it
+      cy.get('a[href*="/"]').click();
+    });
+
     it('should be able to change projector settings', () => {
       // Start from the index page
       cy.visit('https://localhost:3000/');
@@ -47,12 +111,17 @@ describe('Login', () => {
       cy.wait(3000);
 
       // Find the slider with id "volume" and press left arrow 3 times
-      cy.get('#volume').click().type('{leftArrow}'.repeat(3));
+      // cy.get('#volume').click().type('{leftArrow}'.repeat(3));
+      cy.get('#volume')
+        .invoke("val", 5)
+        .trigger("change");
 
       cy.wait(3000);
 
       // Find the slider with id "brightness" and press left arrow 3 times
-      cy.get('#brightness').click().type('{leftArrow}'.repeat(3));
+      cy.get('#brightness')
+        .invoke("val", 80)
+        .trigger("change");
 
       cy.wait(3000);
 
@@ -66,30 +135,10 @@ describe('Login', () => {
 
       // Find all color selectors and change them
       cy.get('input[type=color]').each(($el, index, $list) => {
-        cy.wrap($el).invoke('val', '#ff0000')
+        cy.wrap($el)
+          .invoke('val', '#ff0000')
           .trigger('change');
         cy.wait(3000);
       });
-
-
-      // // Find the switch with id "showSecondSwitch" and click
-      // cy.get('#showSecondSwitch').click();
-
-      // cy.wait(3000);
-
-      // // Find the switch with id "use12HSwitch" and click
-      // cy.get('#use12HSwitch').click();
-
-      // cy.wait(3000);
-
-      // // Find the slider with id "fontSize" and press left arrow 3 times
-      // cy.get('#fontSize').type('{leftArrow}'.repeat(3));
-
-      // cy.wait(3000);
-
-      // // Find the switch with id "showSettingsBarSwitch" and click
-      // cy.get('#showSettingsBarSwitch').click();
-
-      // cy.wait(3000);
     });
   });
